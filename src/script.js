de48d16551c185d5d8ea86aaac5066e0cf10dcb1
Tainá <141168586+taifarias@users.noreply.github.com>
2024-08-12 05:26:15 -0300

    commit JS pronto
    A parte javaScript está completa. Agora resta apenas o design

--- src/script.js
+++ src/script.js
@@ -9,6 +9,54 @@ const buttonComplete = document.getElementById('buttonComplete');
 
 
 
+const getAdress = async (cep) => { 
+
+    const apiUrl =`https://viacep.com.br/ws/${cep}/json/`;
+
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    console.log(data)
+
+
+    if(data.erro) {
+        inputCEP.value = "";
+        alert("CEP invalido")
+
+        
+
+        return;
+    }
+
+    inputRua.value = data.logradouro;
+    inputBairro.value = data.bairro;
+    inputCidade.value = data.localidade;   
+    inputEstado.value = data.uf
+
+    toggleDisabled();
+}
+
+const toggleDisabled = () => {
+    if(inputRua.value.trim() !== "") {
+       
+        inputNum.removeAttribute('disabled');
+        inputCompl.removeAttribute('disabled');
+          
+    } else {
+        // Desabilita os campos se a rua estiver vazia
+        inputNum.setAttribute('disabled', 'true');
+        inputCompl.setAttribute('disabled', 'true');
+    }
+}
+
+const clearInputs = () => {
+    inputRua.value = "";
+    inputBairro.value = "";
+    inputCidade.value = "";
+    inputEstado.value = "";
+}
+
+
 inputCEP.addEventListener('keypress', (e) => {
     
     const onlyNumbers = /[0-9]/;
@@ -25,63 +73,36 @@ inputCEP.addEventListener('keypress', (e) => {
 inputCEP.addEventListener('keyup', (e) => {
     const inputValue = e.target.value;
   
-    if(inputValue.length === 8){
+    if(inputValue.length === 8){    //executar quando tiver 8 digitos
         console.log(inputValue);
-        getAdress(inputValue);
+     getAdress(inputValue);
+    }
+    else{
+        clearInputs();
+
+        toggleDisabled();
+
     }
 });
 
-const getAdress = async (cep) => {
-  
-
-    inputCEP.blur();
-    const apiUrl =`https://viacep.com.br/ws/${cep}/json/`;
-
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-
-    console.log(data)
-
-
-    if(data.erro === "true") {
-        inputCEP.value = "";
-
-        alert("CEP invalido")
-
-
-        return;
-    }
-
-    inputRua.value = data.logradouro;
-    inputBairro.value = data.bairro;
-    inputCidade.value = data.localidade;   
-    inputEstado.value = data.uf
-
-    toggleDisabled();
-
-}
-
 
 buttonComplete.addEventListener("click", (e) => {
-    if(inputRua === "") {
-        alert('Endereço cadastrado com sucesso!');
 
-        inputCEP.value = "";
-
+    if(inputNum.value.trim() === "") {
+        alert('Endereço não preenchido!');
+        return;
     }
+   
     
+    alert('Endereço cadastrado com sucesso!');
+    clearInputs();  
+    inputNum.value = "";
+    inputCEP.value = "";
+    inputCompl.value = "";
+    toggleDisabled();
+
+
+
 })
 
-const toggleDisabled = () => {
-    if(inputRua !== "") {
-       inputRua.toggleAttribute('disabled');
-       inputNum.toggleAttribute('disabled');
-       inputCompl.toggleAttribute('disabled');
-       inputBairro.toggleAttribute('disabled');
-       inputCidade.toggleAttribute('disabled');
-       inputEstado.toggleAttribute('disabled');
-
-        
-    }
-}
 
